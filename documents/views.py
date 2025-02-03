e0daa1c9b456faeefd79fe8e7249bd66e0314949
sumitbansode360 <sumitbansode360@gmail.com>
2025-02-03 21:15:23 +0530

    permission added to views

--- documents/views.py
+++ documents/views.py
@@ -2,23 +2,31 @@ from django.shortcuts import get_object_or_404
 from .models import Document
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class DocListView(ListView):
+
+class DocListView(LoginRequiredMixin, ListView):
     model = Document
     template_name = 'documents/document_list.html'
+    login_url = 'login'
 
-class DocCreateView(CreateView):
+
+class DocCreateView(LoginRequiredMixin, CreateView):
     model = Document
     template_name = 'documents/document_create.html'
+    login_url = 'login'
+
     fields = ['title']
 
     def get_success_url(self):
         return reverse_lazy('doc-edit', kwargs={'uuid': self.object.uuid})
 
 
-class DocDeleteView(DeleteView):
+class DocDeleteView(LoginRequiredMixin, DeleteView):
     model = Document
     template_name = 'documents/document_delete.html'
+    login_url = 'login'
+
 
     lookup_field = 'uuid'
 
@@ -29,9 +37,11 @@ class DocDeleteView(DeleteView):
     success_url = reverse_lazy('doc-list')
 
 
-class DocUpdateView(UpdateView):
+class DocUpdateView(LoginRequiredMixin, UpdateView):
     model = Document
     template_name = 'documents/document_update.html'
+    login_url = 'login'
+
     fields = ['title', 'content']
     # Override lookup field to use UUID instead of default 'pk'
     lookup_field = 'uuid'
